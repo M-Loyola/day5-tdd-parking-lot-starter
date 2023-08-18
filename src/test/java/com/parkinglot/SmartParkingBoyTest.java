1021403d5c59a40ba31943464ea50b3fd3a085e7
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-18 20:16:39 +0800

    feat: add test and implementation should_park_to_first_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_and_a_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -1,2 +1,24 @@
-package com.parkinglot;public class SmartParkingBoyTest {
+package com.parkinglot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class SmartParkingBoyTest {
+    @Test
+    void should_park_to_first_parking_lot_when_park_given_a_standard_parking_boy_and_two_parking_lots_and_a_car() {
+        //Given
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+        //When
+        ParkingTicket parkingTicket = smartParkingBoy.park(car);
+        //Then
+        Assertions.assertNotNull(parkingTicket);
+        Assertions.assertEquals(9, firstParkingLot.getAvailableCapacity());
+        Assertions.assertEquals(10, secondParkingLot.getAvailableCapacity());
+    }
 }
