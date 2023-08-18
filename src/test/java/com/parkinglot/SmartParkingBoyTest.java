b9de993ffc4b0bf86785f53e510739da56e7fef0
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-18 20:19:08 +0800

    feat: add test should_park_to_second_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_first_parking_lot_is_full_and_a_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -3,16 +3,17 @@ package com.parkinglot;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SmartParkingBoyTest {
+    ParkingLot firstParkingLot = new ParkingLot();
+    ParkingLot secondParkingLot = new ParkingLot();
+    List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+    SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
     @Test
-    void should_park_to_first_parking_lot_when_park_given_a_standard_parking_boy_and_two_parking_lots_and_a_car() {
+    void should_park_to_first_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_and_a_car() {
         //Given
-        ParkingLot firstParkingLot = new ParkingLot();
-        ParkingLot secondParkingLot = new ParkingLot();
-        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
-        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
         Car car = new Car();
         //When
         ParkingTicket parkingTicket = smartParkingBoy.park(car);
@@ -20,5 +21,24 @@ public class SmartParkingBoyTest {
         Assertions.assertNotNull(parkingTicket);
         Assertions.assertEquals(9, firstParkingLot.getAvailableCapacity());
         Assertions.assertEquals(10, secondParkingLot.getAvailableCapacity());
+    }
+    @Test
+    void should_park_to_second_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_first_parking_lot_is_full_and_a_car() {
+        //Given
+        List<Car> cars = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            cars.add(new Car());
+        }
+
+        for (Car car : cars) {
+            firstParkingLot.park(car);
+        }
+        //When
+        ParkingTicket parkingTicket = smartParkingBoy.park(new Car());
+        //Then
+        Assertions.assertNotNull(parkingTicket);
+        Assertions.assertEquals(0, firstParkingLot.getAvailableCapacity());
+        Assertions.assertEquals(9, secondParkingLot.getAvailableCapacity());
     }
 }
