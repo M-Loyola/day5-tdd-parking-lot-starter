4cea869574a3dc266801280ac73fb8389c3e5d06
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-18 15:32:32 +0800

    refactor: modify implementation create separate methods

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -4,22 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
+    public static final int DEFAULT_CAPACITY = 10;
     private final Map<ParkingTicket, Car> cars = new HashMap<>();
 
     private final int capacity;
 
     public ParkingLot() {
-        this.capacity = 10;
+        this.capacity = DEFAULT_CAPACITY;
     }
     public ParkingTicket park(Car car) {
         ParkingTicket parkingTicket = new ParkingTicket();
-        if (cars.size() == capacity) return null;
+        if (isParkingLotFull()) return null;
         cars.put(parkingTicket,car);
         return parkingTicket;
     }
+
+    private boolean isParkingLotFull() {
+        return cars.size() == capacity;
+    }
+
     public Car fetch (ParkingTicket parkingTicket) {
-        Car fetchedCar = cars.get(parkingTicket);
-        cars.remove(parkingTicket);
-        return fetchedCar;
+        return  cars.remove(parkingTicket);
     }
 }
